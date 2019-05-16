6b6212338964dcbccd8eddc499489bac920f7d5c
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2019-05-16 15:47:29 +0100

    sample of exerc1 added

--- 1-intro-shiny/app-exercise1.R
+++ 1-intro-shiny/app-exercise1.R
@@ -1,4 +1,17 @@
 library(shiny)
+library(shinydashboard)
 
 # Your code here
 # Good luck :-)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) {
+  
+}
+
+shinyApp(ui, server)
